f22ec84c899125a4b8df15a53de4760c56862892
Arulraj Gopal <arulrajgopal@outlook.com>
2025-08-02 22:20:49 +0530

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "test_storage_account"
+  name                     = "arulteststorageacct"
   resource_group_name       = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
   account_tier              = "Standard"
@@ -29,7 +29,7 @@ resource "azurerm_storage_account" "storage_account" {
 }
 
 resource "azurerm_data_factory" "adf" {
-  name                = "azuredatafactory6789"
+  name                = "azuredatafactory"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
 
