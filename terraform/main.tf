f998c0cdfe3dcdc0a5322ecea347d67c96548733
arulrajgopal-zerotoone <gopalarulraj@gmail.com>
2025-08-09 13:06:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,30 +48,48 @@ resource "azurerm_databricks_workspace" "dbx" {
   }
 }
 
-# # azuread provider setup
-# provider "azuread" {
-#  tenant_id       = var.tenant_id
-#  client_id       = var.client_id
-#  client_secret   = var.client_secret
-# }
+# azuread provider setup
+provider "azuread" {
+ tenant_id       = var.tenant_id
+ client_id       = var.client_id
+ client_secret   = var.client_secret
+}
 
-# data "azuread_client_config" "current" {}
 
-# resource "azuread_application" "ad_app" {
-#   display_name = "spn6789"
-#   owners       = [data.azuread_client_config.current.object_id]
-# }
+# Create an Azure AD application and service principal
+# to assign the Storage Blob Data Contributor role
+data "azuread_client_config" "current" {}
 
-# resource "azuread_service_principal" "spn" {
-#   client_id                    = azuread_application.ad_app.client_id
-#   app_role_assignment_required = false
-#   owners                       = [data.azuread_client_config.current.object_id]
-# }
+resource "azuread_application" "ad_app" {
+  display_name = "spn6789"
+  owners       = [data.azuread_client_config.current.object_id]
+}
 
-# resource "azurerm_role_assignment" "spn_blob_contributor" {
-#   scope                = azurerm_resource_group.resource_group.id
-#   role_definition_name = "Storage Blob Data Contributor"
-#   principal_id         = azuread_service_principal.spn.object_id
-# }
+resource "azuread_service_principal" "spn" {
+  client_id                    = azuread_application.ad_app.client_id
+  app_role_assignment_required = false
+  owners                       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azurerm_role_assignment" "spn_blob_contributor" {
+  scope                = azurerm_resource_group.resource_group.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azuread_service_principal.spn.object_id
+}
+
+
+# Assign the Owner role to a specific user
+data "azuread_user" "target_user" {
+  user_principal_name = "User-2@arulrajgopaloutlook.onmicrosoft.com"
+}
+
+
+resource "azurerm_role_assignment" "owner_assignment" {
+  scope                = "/subscriptions/52414a32-8b98-454a-a605-c388463d2d8e"
+  role_definition_name = "Owner"
+  principal_id         = data.azuread_user.target_user.object_id
+}
+
+
 
 
